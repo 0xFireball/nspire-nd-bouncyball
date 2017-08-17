3a03505c8fcfb3a3c77bbec438531c4f8e2c5779
0xFireball <0xFireball@outlook.com>
2017-08-16 21:11:50 -0700

    add NEntity

---
+++ nd/entity/NEntity.cpp
@@ -0,0 +1,6 @@
+
+#include "NEntity.h"
+
+void NEntity::init() {
+    // TODO
+}
--- nd/entity/NEntity.h
+++ nd/entity/NEntity.h
@@ -4,5 +4,15 @@
 #include "NBasic.h"
 
 class NEntity : public NBasic {
-    
-}
+  protected:
+    void init();
+
+  public:
+    int x;
+    int y;
+    int width;
+    int height;
+
+    NEntity(int x = 0, int y = 0, int width = 0, int height = 0)
+        : NBasic(), x(x), y(y), width(width), height(height) {}
+};
